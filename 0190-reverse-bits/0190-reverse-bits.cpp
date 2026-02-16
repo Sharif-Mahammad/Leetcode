ed0940eebf0e549df3173ff1b524bc7e25520b10
Sharif-Mahammad <mohamaddshariffff@gmail.com>
2026-02-16 11:48:47 +0530

    Time: 0 ms (100%), Space: 8.3 MB (46.07%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        bitset<32>b(n);
+
+        string s = b.to_string();
+        reverse(s.begin(), s.end());
+
+        bitset<32>rev(s);
+        long ans=rev.to_ulong();
+        
+        return ans;
+    }
+};
