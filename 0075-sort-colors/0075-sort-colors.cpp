136b94b8d425a40aa74ab02fcc81916fab340936
Sharif-Mahammad <mohamaddshariffff@gmail.com>
2026-06-19 10:18:07 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.06%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& a) {
+        int l=0, m=0, h=a.size();
+        h=h-1;
+        while(m <= h){
+            if(a[m]==0){
+                swap(a[m], a[l]);
+                l++;
+                m++;
+            }
+            else if(a[m]==1){
+                m++;
+            }
+            else{
+                swap(a[m], a[h]);
+                h--;
+            }
+        }
+    }
+};
