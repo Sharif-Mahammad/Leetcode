e90e72930f0c8a27fbcd4ec2ec1fe51331b3746c
Sharif <mohamaddshariffff@gmail.com>
2026-08-12 19:25:15 +0530

    Time: 0 ms (100%), Space: 23 MB (23.77%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int distinctPrimeFactors(vector<int>& nums) {
         int n = *max_element(nums.begin(), nums.end());
-        
+
         vector<int>sieve;
         for(int i=0; i<=n; i++){
             sieve.push_back(i);
@@ -18,15 +18,11 @@ public:
         
         unordered_set<int>prem;
 
-        for(int i=0; i<nums.size(); i++){
-            int numerator = nums[i];
-            int fact = sieve[numerator];
-            while(numerator / fact != 1){
-                prem.insert(fact);
-                numerator = numerator / fact;
-                fact = sieve[numerator];
+        for(int x: nums){
+            while(x>1){
+                prem.insert(sieve[x]);
+                x /= sieve[x];
             }
-            prem.insert(fact);
         }
         return prem.size();
         
